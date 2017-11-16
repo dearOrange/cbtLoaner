ce3bb7716fd58703de2898bcec77902d950dbb62
jiaguishan <jiaguishan@gmail.com>
2017-11-16 14:02:36 +0800

    增加公共配置 更新文件

---
+++ js/common.js
@@ -0,0 +1,10 @@
+var cbt = cbt || {
+    version: '1.0.0'
+};
+cbt.utils = cbt.utils || {}; //公共函数
+
+var ROOTURL = '/wx';/*项目根目录名称 谨慎修改*/
+var REQUESTROOT = '/hunterServer'; /*服务器默认为/manager 当本地开发时切换为域名映射*/
+if(window.location.hostname === 'local.cbt.com'){
+    REQUESTROOT = 'http://test.cbt.com:8080/hunterServer';
+}
