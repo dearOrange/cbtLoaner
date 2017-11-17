c602c431a1812286aae8e7ee8dc8ed92795bfb56
jiaguishan <jiaguishan@gmail.com>
2017-11-17 20:09:51 +0800

    微信

--- js/common.js
+++ js/common.js
@@ -3,16 +3,16 @@ var cbt = cbt || {
 };
 cbt.utils = cbt.utils || {}; //公共函数
 
-var ROOTURL = '/hunter'; /*项目根目录名称 谨慎修改*/
+var ROOTURL = ''; /*项目根目录名称 谨慎修改*/
 var REQUESTROOT = '/hunterServer'; /*服务器默认为/manager 当本地开发时切换为域名映射*/
-var qiniuURL = '';
-if (window.location.hostname === 'local.cbt.com') {
-    REQUESTROOT = 'http://test.cbt.com:8080/hunterServer';
-    qiniuURL = 'http://test.cbt.com:8080/adminServer';
-} else {
-    REQUESTROOT = 'http://192.168.2.181:8080/hunterServer';
-    qiniuURL = 'http://192.168.2.181:8080/adminServer';
-}
+var qiniuURL = '/adminServer';
+// if (window.location.hostname === 'local.cbt.com') {
+//     REQUESTROOT = 'http://test.cbt.com:8080/hunterServer';
+//     qiniuURL = 'http://test.cbt.com:8080/adminServer';
+// } else {
+//     REQUESTROOT = 'http://192.168.2.181:8080/hunterServer';
+//     qiniuURL = 'http://192.168.2.181:8080/adminServer';
+// }
 $.ajaxSetup({
     beforeSend: function(xhr) {
         var token = $.cookie('X-Token');
