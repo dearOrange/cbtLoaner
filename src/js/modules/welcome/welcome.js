5d490fab6cb1b82c469525bd3a03093d8dc2ff81
pengjun <pengjun1216@163.com>
2018-01-05 17:54:09 +0800

    找回车辆滚动

--- src/js/modules/welcome/welcome.js
+++ src/js/modules/welcome/welcome.js
@@ -9,6 +9,7 @@ define(function(require, exports, module) {
     function WelcomeDetail() {
         this.init = function() {
             this.registerEvent();
+            this.scrollEvent();
         };
         this.registerEvent = function() {
             jh.utils.uploader.init({
@@ -35,6 +36,33 @@ define(function(require, exports, module) {
                 jh.utils.load("/src/modules/find/find-list");
             });
         };
+        this.scrollEvent =  function(){
+        	var carinfoItem = document.getElementById('carinfo-item');
+        	var carinfoFirst = document.getElementById('carinfo-first');
+        	var carinfoSecond = document.getElementById('carinfo-second');
+        	carinfoSecond.innerHTML = carinfoFirst.innerHTML;
+        	carinfoFirst.style.height = carinfoItem.offsetHeight + 'px';
+        	carinfoSecond.style.height = carinfoItem.offsetHeight + 'px';
+        	var timer = window.setInterval(function(){
+        		if(carinfoItem.scrollTop >= carinfoItem.offsetHeight) {
+        			carinfoItem.scrollTop = 0;
+       			}else{
+       				carinfoItem.scrollTop++;
+       			}
+        	},50)
+        	carinfoItem.onmouseover = function(){
+        		clearInterval(timer);
+        	}
+        	carinfoItem.onmouseout = function(){
+        		timer = window.setInterval(function(){
+	        		if(carinfoItem.scrollTop >= carinfoItem.offsetHeight) {
+	        			carinfoItem.scrollTop = 0;
+	       			}else{
+	       				carinfoItem.scrollTop++;
+	       			}
+	        	},50)
+        	}
+        }
     }
     module.exports = WelcomeDetail;
 });
