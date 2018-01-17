ccec92fc0685e00dd4cf7cd0bc60c2cca958824c
pengjun <pengjun1216@163.com>
2018-01-17 17:35:55 +0800

    更新

--- src/js/modules/find/find-list.js
+++ src/js/modules/find/find-list.js
@@ -48,19 +48,6 @@ define(function(require, exports, module) {
                     var alertStr = jh.utils.template('find-detail-template', returnData);
                     var okStr = returnData.data.state === 'platReceive' ? '收到车了' : '确定';
                     
-                    jh.utils.uploader.init({
-                        fileNumLimit: 15,
-                        pick: {
-                            id: '#voucherUrl'
-                        }
-                    });
-
-                    jh.utils.uploader.init({
-                        fileNumLimit: 5,
-                        pick: {
-                            id: '#entrustUrl'
-                        }
-                    });
                     
                     jh.utils.alert({
                         content: alertStr,
@@ -82,6 +69,19 @@ define(function(require, exports, module) {
                         cancel: true
                     });
 
+                    jh.utils.uploader.init({
+                        fileNumLimit: 15,
+                        pick: {
+                            id: '#voucherUrl'
+                        }
+                    });
+
+                    jh.utils.uploader.init({
+                        fileNumLimit: 5,
+                        pick: {
+                            id: '#entrustUrl'
+                        }
+                    });
                 }
             });
         };
