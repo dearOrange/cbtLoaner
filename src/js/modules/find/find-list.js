b5f8b987c5e40cc7865e021dd09021e8bed89306
jiaguishan <jiaguishan@chebutou.com.cn>
2018-03-16 11:21:28 +0800

    电子签章跳过

--- src/js/modules/find/find-list.js
+++ src/js/modules/find/find-list.js
@@ -6,203 +6,243 @@
  */
 'use strict';
 define(function(require, exports, module) {
-    function FindList() {
-        var _this = this;
-        _this.data = '';
-        _this.form = $('#custmoer-findList-form');
-        this.init = function() {
-            this.initContent();
-            this.registerEvent();
-            $('select').select2({
-                minimumResultsForSearch: Infinity
-            });
-        };
-        this.initContent = function(isSearch) {
-            var datas = jh.utils.formToJson(_this.form);
-            datas.carNumber = datas.carNumber_province + datas.carNumber;
-            delete datas.carNumber_province;
-            var page = new jh.ui.page({
-                data_container: $('#find_list_container'),
-                page_container: $('#page_container'),
-                method: 'post',
-                url: '/task/findedList',
-                contentType: 'application/json',
-                data: datas,
-                isSearch: isSearch,
-                callback: function(returnData) {
-                    return jh.utils.template('findList-contentTemplate', returnData);
+  function FindList() {
+    var _this = this;
+    _this.data = '';
+    _this.form = $('#custmoer-findList-form');
+
+    var auth = sessionStorage.getItem('customer-isState');
+
+    this.init = function() {
+      this.initContent();
+      this.registerEvent();
+      $('select').select2({
+        minimumResultsForSearch: Infinity
+      });
+    };
+    this.initContent = function(isSearch) {
+      var datas = jh.utils.formToJson(_this.form);
+      datas.carNumber = datas.carNumber_province + datas.carNumber;
+      delete datas.carNumber_province;
+      var page = new jh.ui.page({
+        data_container: $('#find_list_container'),
+        page_container: $('#page_container'),
+        method: 'post',
+        url: '/task/findedList',
+        contentType: 'application/json',
+        data: datas,
+        isSearch: isSearch,
+        callback: function(returnData) {
+          return jh.utils.template('findList-contentTemplate', returnData);
+        }
+      });
+      page.init();
+    };
+
+    //展示详情
+    this.showDetail = function(taskId) {
+      jh.utils.ajax.send({
+        url: '/task/detail',
+        data: {
+          taskId: taskId
+        },
+        done: function(returnData) {
+          returnData.menuState = jh.utils.menuState;
+          returnData.viewImgRoot = jh.config.viewImgRoot;
+          var alertStr = jh.utils.template('find-detail-template', returnData);
+          var okStr = (returnData.data.state === 'platReceive' || returnData.data.state === 'hunterReceive') ? '收到车了' : '确定';
+          _this.data = returnData.data;
+
+          okStr = returnData.data.state === 'unconfirmed' ? '电子签章' : okStr;
+
+          var alertOption = {
+            content: alertStr,
+            okValue: okStr,
+            ok: function() {
+              if (auth === 'available') {
+                //如果是待债权方确认状态则进行上传凭证与进行电子签章
+                if (_this.data.state === "unconfirmed" || _this.data.state === "hunterUnreceive") {
+                  _this.uploadVouch(_this.data);
+                  return false;
+                } else if (_this.data.state === 'platReceive' || _this.data.state === "hunterReceive") {
+                  (new jh.ui.shadow()).init();
+                  //如果平台已收车，则债权方进行确认收车
+                  _this.confirmeReceive(taskId);
+                  return false;
+                } else {
+                  return true;
                 }
-            });
-            page.init();
-        };
-
-        //展示详情
-        this.showDetail = function(taskId) {
-            jh.utils.ajax.send({
-                url: '/task/detail',
-                data: {
-                    taskId: taskId
-                },
-                done: function(returnData) {
-                    returnData.menuState = jh.utils.menuState;
-                    returnData.viewImgRoot = jh.config.viewImgRoot;
-                    var alertStr = jh.utils.template('find-detail-template', returnData);
-                    var okStr = returnData.data.state === 'platReceive' || returnData.data.state === 'hunterReceive' ? '收到车了' : '确定';
-                    _this.data = returnData.data;
-                    
-                    jh.utils.alert({
-                        content: alertStr,
-                        okValue: okStr,
-                        ok: function() {
-                            //如果是待债权方确认状态则进行上传凭证与进行电子签章
-                            if (_this.data.state === "unconfirmed" || _this.data.state === "hunterUnreceive") {
-                                _this.uploadVouch(_this.data);
-                                return false;
-                            } else if (_this.data.state === 'platReceive' || _this.data.state === "hunterReceive") {
-                                (new jh.ui.shadow()).init();
-                                //如果平台已收车，则债权方进行确认收车
-                                _this.confirmeReceive(taskId);
-                                return false;
-                            } else {
-			                    return true;
-			                }
-                        },
-                        cancel: true
-                    });
-
-                    jh.utils.uploader.init({
-                        fileNumLimit: 15,
-                        pick: {
-                            id: '#voucherUrl'
-                        }
-                    });
-
-                    jh.utils.uploader.init({
-                        fileNumLimit: 5,
-                        pick: {
-                            id: '#entrustUrl'
-                        }
-                    });
-                }
-            });
-        };
-
-        //上传凭证
-        this.uploadVouch = function(returnDetail) {
-            var datas = jh.utils.formToJson($('#custmoer-upload-form'));
-            datas.type = returnDetail.state === 'unconfirmed'? 0 : 1;
-            //判断有无上传凭证
-            if (!datas.voucherUrl) {
-                jh.utils.confirm({
-                    content: '请上传相关凭证！',
-                    ok: true,
-                    cancel: true
-                });
-                return false;
+              } else {
+                jh.utils.load('/src/modules/person/person-center');
+                return true;
+              }
             }
-            if (!datas.entrustUrl) {
-                jh.utils.confirm({
-                    content: '请上传委托书原件！',
-                    ok: true,
-                    cancel: true
-                });
+          };
+
+          if (_this.data.state === "unconfirmed") {
+            alertOption.button = [{
+              value: '直接确认拖车',
+              callback: function() {
+                _this.uploadVouch(_this.data, 'skip');
+
                 return false;
+              }
+            }];
+          } else {
+            alertOption.cancel = true;
+          }
+
+          jh.utils.alert(alertOption);
+
+          jh.utils.uploader.init({
+            fileNumLimit: 15,
+            pick: {
+              id: '#voucherUrl'
             }
-            //以逗号连接数据
-            datas.voucherUrl = jh.utils.isArray(datas.voucherUrl) ? datas.voucherUrl.join(',') : datas.voucherUrl;
-            datas.entrustUrl = jh.utils.isArray(datas.entrustUrl) ? datas.entrustUrl.join(',') : datas.entrustUrl;
-            (new jh.ui.shadow()).init();
-            //保存已上传凭证数据
+          });
+
+          jh.utils.uploader.init({
+            fileNumLimit: 5,
+            pick: {
+              id: '#entrustUrl'
+            }
+          });
+        }
+      });
+    };
+
+    //上传凭证
+    this.uploadVouch = function(returnDetail, skip) {
+      var datas = jh.utils.formToJson($('#custmoer-upload-form'));
+      datas.type = returnDetail.state === 'unconfirmed' ? 0 : 1;
+      //判断有无上传凭证
+      if (!datas.voucherUrl) {
+        jh.utils.confirm({
+          content: '请上传相关凭证！',
+          ok: true,
+          cancel: true
+        });
+        return false;
+      }
+      if (!datas.entrustUrl) {
+        jh.utils.confirm({
+          content: '请上传委托书原件！',
+          ok: true,
+          cancel: true
+        });
+        return false;
+      }
+      //以逗号连接数据
+      datas.voucherUrl = jh.utils.isArray(datas.voucherUrl) ? datas.voucherUrl.join(',') : datas.voucherUrl;
+      datas.entrustUrl = jh.utils.isArray(datas.entrustUrl) ? datas.entrustUrl.join(',') : datas.entrustUrl;
+      (new jh.ui.shadow()).init();
+      //保存已上传凭证数据
+      jh.utils.ajax.send({
+        url: '/task/confirmeEntrust',
+        method: 'post',
+        data: datas,
+        done: function(returnData) {
+          jh.utils.closeArt();
+          if (skip) {
             jh.utils.ajax.send({
-                url: '/task/confirmeEntrust',
-                method: 'post',
-                data: datas,
-                done: function(returnData) {
+              url: '/task/skip',
+              data: {
+                taskId: taskId
+              },
+              done: function() {
+                jh.utils.alert({
+                  content: '操作成功！',
+                  ok: function() {
                     jh.utils.closeArt();
-                    if (returnDetail.contractState === 0) {
-                        //保存成功后调用电子签章
-                        _this.requestContractUrl(datas.taskId);
-                    } else {
-                        (new jh.ui.shadow()).close();
-                        window.location.reload();
-                    }
-                }
+                  }
+                });
+              }
             });
-        };
+          }
+          if (returnDetail.contractState === 0) {
+            //保存成功后调用电子签章
+            _this.requestContractUrl(datas.taskId);
+          } else {
+            (new jh.ui.shadow()).close();
+            window.location.reload();
+          }
+        }
+      });
+    };
 
-        //电子签章
-        this.requestContractUrl = function(taskId) {
-            jh.utils.ajax.send({
-                url: '/tasksign/getContractUrl',
-                data: {
-                    taskId: taskId
-                },
-                done: function(returnData) {
-                    var width = $(window).width() * 0.95;
-                    var height = $(window).height() * 0.9;
-                    (new jh.ui.shadow()).close();
-                    //调用第三方电子签章
-                    dialog({
-                        url: returnData.data.signUrl,
-                        id: 'iframe-dialog',
-                        width: width,
-                        height: height,
-                        cancel: function(){
-                        	window.location.reload();
-                        },
-                        cancelValue: '关闭',
-                        fixed: true,
-                        onclose: function() {
-                            window.location.reload();
-                        }
-                    }).showModal();
-                }
-            });
-        };
+    //电子签章
+    this.requestContractUrl = function(taskId) {
+      jh.utils.ajax.send({
+        url: '/tasksign/getContractUrl',
+        data: {
+          taskId: taskId
+        },
+        done: function(returnData) {
+          var width = $(window).width() * 0.95;
+          var height = $(window).height() * 0.9;
+          (new jh.ui.shadow()).close();
+          //调用第三方电子签章
+          dialog({
+            url: returnData.data.signUrl,
+            id: 'iframe-dialog',
+            width: width,
+            height: height,
+            cancel: function() {
+              window.location.reload();
+            },
+            cancelValue: '关闭',
+            fixed: true,
+            onclose: function() {
+              window.location.reload();
+            }
+          }).showModal();
+        }
+      });
+    };
 
-        //确认收车
-        this.confirmeReceive = function(taskId) {
-            //如果平台收到车了，则进行债权方确认收车操作
-            jh.utils.ajax.send({
-                url: '/task/confirmeReceive',
-                data: {
-                    taskId: taskId
-                },
-                done: function(urlData) {
-                    jh.utils.closeArt();
-                    (new jh.ui.shadow()).close();
-                    jh.utils.alert({
-                        content: '恭喜收到爱车！',
-                        ok: function() {
-                            window.location.reload();
-                        },
-                        cancel: false
-                    });
-                }
-            });
-        };
+    //确认收车
+    this.confirmeReceive = function(taskId) {
+      //如果平台收到车了，则进行债权方确认收车操作
+      jh.utils.ajax.send({
+        url: '/task/confirmeReceive',
+        data: {
+          taskId: taskId
+        },
+        done: function(urlData) {
+          jh.utils.closeArt();
+          (new jh.ui.shadow()).close();
+          jh.utils.alert({
+            content: '恭喜收到爱车！',
+            ok: function() {
+              window.location.reload();
+            },
+            cancel: false
+          });
+        }
+      });
+    };
 
-        this.registerEvent = function() {
-            //查看任务详情
-            $('.dataShow').off('click', '.detail').on('click', '.detail', function() {
-                var me = $(this);
-                var taskId = me.data('id');
-                _this.showDetail(taskId);
-            });
+    this.registerEvent = function() {
+      //查看任务详情
+      $('.dataShow').off('click', '.detail').on('click', '.detail', function() {
+        var me = $(this);
+        var taskId = me.data('id');
+        _this.showDetail(taskId);
+      });
 
-            // 搜索
-            jh.utils.validator.init({
-                id: 'custmoer-findList-form',
-                submitHandler: function(form) {
-                    _this.initContent(true);
-                    return false;
-                }
-            });
-            
-			$('body').off('click', '.upload-voList').on('click', '.upload-voList', function() {
-                _this.uploadVouch(_this.data);
-            });
-        };
-    }
-    module.exports = FindList;
+      // 搜索
+      jh.utils.validator.init({
+        id: 'custmoer-findList-form',
+        submitHandler: function(form) {
+          _this.initContent(true);
+          return false;
+        }
+      });
+
+      $('body').off('click', '.upload-voList').on('click', '.upload-voList', function() {
+        _this.uploadVouch(_this.data);
+      });
+    };
+  }
+  module.exports = FindList;
 });
